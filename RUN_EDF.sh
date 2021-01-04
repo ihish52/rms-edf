ec7ce41ef9055c55723ce73ae7c0f58be1cd335c
hmrp1r17 <hmrp1r17@soton.ac.uk>
2021-01-04 12:55:32 +0000

    minor commit

--- RUN_EDF.sh
+++ RUN_EDF.sh
@@ -1,4 +1,5 @@
 echo "Generating Output File"
+g++ edf.cpp -o edf.exe
 ./edf.exe
 echo "Displaying EDF Plot"
 python3 plot.py output_edf.txt
--- RUN_RMS.sh
+++ RUN_RMS.sh
@@ -1,4 +1,5 @@
 echo "Generating Output File"
+g++ rms.cpp -o rms.exe
 ./rms.exe
 echo "Displaying RMS Plot"
 python3 plot.py output_rms.txt
